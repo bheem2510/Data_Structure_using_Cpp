4f9affd945cd9b3e85a1f5b9f759bc3f14e2be6f
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-09-01 18:07:52 +0530

    Add files via upload

---
+++ Sorting/Recursion_Sorting.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void Recurtion_Sort(int arr[], int size){
+	
+	if(size==1)
+	return;
+
+
+for(int i=0; i<size-1; i++){
+	if(arr[i] > arr[i+1]){
+		swap(arr[i], arr[i+1]);
+	}
+	Recurtion_Sort(arr, size-1);
+}
+}
+
+void Print_array(int arr[],int size){
+	cout<<"Printing array after recurion sorting: "<<endl;
+	for(int i=0; i<size; i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+//-----------------------------------------------------
+
+int main(){
+	int arr[] = {34,23,12,85,3,8,6};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	Recurtion_Sort(arr,size);
+	Print_array(arr,size);
+	return 0;
+}
